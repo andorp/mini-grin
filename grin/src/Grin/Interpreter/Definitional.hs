6fba0e1e6e66e43401ceb80a86be841cb033bec8
Andor Penzes <andor.penzes@gmail.com>
2019-07-02 00:25:09 +0200

    Def: Rename NHVal to DVal.

--- grin/src/Grin/Interpreter/Definitional.hs
+++ grin/src/Grin/Interpreter/Definitional.hs
@@ -10,7 +10,20 @@ import Grin.Interpreter.Base
 import Lens.Micro.Platform
 import qualified Data.Map as Map
 
+
 -- * NoHeap interpreter
+
+
+data Node = Node Tag [Lit]
+  deriving (Eq, Ord, Show)
+
+data DVal
+  = DNode Node
+  | DVal  Lit
+  | DUnit
+  | DLoc  Int
+  deriving (Eq, Ord, Show)
+
 
 data NoHeapEnv m v = NoHeapEnv
   { _noHeapFuns :: Map.Map Name Exp
@@ -20,37 +33,31 @@ data NoHeapEnv m v = NoHeapEnv
 
 makeLenses ''NoHeapEnv
 
-newtype NoHeapT m a = NoHeapT { noHeapT :: ReaderT (NoHeapEnv m NHVal) m a }
+newtype NoHeapT m a = NoHeapT { noHeapT :: ReaderT (NoHeapEnv m DVal) m a }
   deriving (Functor, Applicative, Monad, MonadFail, MonadIO)
 
-runNoHeapT :: (Monad m) => Exp -> [(Name, [NHVal] -> m NHVal)] -> NoHeapT m a -> m a
+runNoHeapT :: (Monad m) => Exp -> [(Name, [DVal] -> m DVal)] -> NoHeapT m a -> m a
 runNoHeapT prog ops n = runReaderT (noHeapT n) noHeapEnv
   where
     (Program _ defs) = prog
     noHeapEnv = NoHeapEnv (Map.fromList ((\d@(Def n _ _) -> (n,d)) <$> defs)) (Map.fromList ops) emptyEnv
 
-data NHVal
-  = NHNode Tag [Lit]
-  | NHVal  Lit
-  | NHUnit
-  | NHLoc  Int
-  deriving (Eq, Ord, Show)
 
 instance (Applicative m, Monad m, MonadFail m) => Interpreter (NoHeapT m) where
-  type IntpVal (NoHeapT m) = NHVal
-  value :: Val -> (NoHeapT m) NHVal
+  type IntpVal (NoHeapT m) = DVal
+  value :: Val -> (NoHeapT m) DVal
   value = \case
     (ConstTagNode t0 ps) -> do
       p  <- askEnv
       vs <- pure $ map (lookupEnv p) ps
-      pure $ NHNode t0 $ map (\(NHVal v) -> v) vs
-    (Lit n) -> pure $ NHVal n
-    Unit    -> pure $ NHUnit
+      pure $ DNode $ Node t0 $ map (\(DVal v) -> v) vs
+    (Lit n) -> pure $ DVal n
+    Unit    -> pure $ DUnit
 
-  askEnv :: (NoHeapT m) (Env NHVal)
+  askEnv :: (NoHeapT m) (Env DVal)
   askEnv = NoHeapT (_noHeapEnv <$> ask)
 
-  localEnv :: Env NHVal -> (NoHeapT m) NHVal -> (NoHeapT m) NHVal
+  localEnv :: Env DVal -> (NoHeapT m) DVal -> (NoHeapT m) DVal
   localEnv e = NoHeapT . local (noHeapEnv %~ (const e)) . noHeapT
 
   lookupFun :: Name -> (NoHeapT m) Exp
@@ -59,31 +66,31 @@ instance (Applicative m, Monad m, MonadFail m) => Interpreter (NoHeapT m) where
   isOperation :: Name -> (NoHeapT m) Bool
   isOperation funName = NoHeapT ((Map.member funName . _noHeapOps) <$> ask)
 
-  operation :: Name -> [NHVal] -> (NoHeapT m) NHVal
+  operation :: Name -> [DVal] -> (NoHeapT m) DVal
   operation funName params = NoHeapT $ do
     op <- (fromJust . Map.lookup funName . _noHeapOps) <$> ask
     lift (op params)
 
-  ecase :: (Exp -> (NoHeapT m) NHVal) -> NHVal -> [Alt] -> (NoHeapT m) NHVal
+  ecase :: (Exp -> (NoHeapT m) DVal) -> DVal -> [Alt] -> (NoHeapT m) DVal
   ecase ev v alts = evalBranch v $ head $ filter (\(Alt p b) -> match v p) alts
     where
-      match :: NHVal -> CPat -> Bool
-      match (NHNode t0 ps) (NodePat t1 vs) = t0 == t1
-      match (NHVal l0)     (LitPat l1)     = l0 == l1
-      match (NHNode{})     DefaultPat      = True
-      match (NHVal{})      DefaultPat      = True
-      match NHUnit         p               = error $ show ("matching failure:", NHUnit, p)
-      match (NHLoc l)      p               = error $ show ("matching failure:", NHLoc l, p)
-      match _                    _         = False
+      match :: DVal -> CPat -> Bool
+      match (DNode (Node t0 ps))  (NodePat t1 vs) = t0 == t1
+      match (DVal l0)             (LitPat l1)     = l0 == l1
+      match (DNode{})             DefaultPat      = True
+      match (DVal{})              DefaultPat      = True
+      match DUnit                 p               = error $ show ("matching failure:", DUnit, p)
+      match (DLoc l)              p               = error $ show ("matching failure:", DLoc l, p)
+      match _                     _               = False
 
-      evalBranch :: NHVal -> Alt -> (NoHeapT m) NHVal
-      evalBranch (NHNode t0 vs) (Alt (NodePat t1 nps) body) = do
+      evalBranch :: DVal -> Alt -> (NoHeapT m) DVal
+      evalBranch (DNode (Node t0 vs)) (Alt (NodePat t1 nps) body) = do
         p <- askEnv
-        let p' = extendEnv p (nps `zip` (NHVal <$> vs))
+        let p' = extendEnv p (nps `zip` (DVal <$> vs))
         localEnv p' (ev body)
-      evalBranch _              (Alt _               body) = ev body
+      evalBranch _                    (Alt _               body) = ev body
 
-evalNoHeap :: (Monad m, MonadFail m, MonadIO m) => Program -> m NHVal
+evalNoHeap :: (Monad m, MonadFail m, MonadIO m) => Program -> m DVal
 evalNoHeap prog = do
   let ops = [ ("prim_int_add", prim_int_add)
             , ("prim_int_sub", prim_int_sub)
@@ -93,11 +100,11 @@ evalNoHeap prog = do
             ]
   runNoHeapT prog ops (eval (grinMain prog))
   where
-    prim_int_add    [(NHVal (LInt64 a)),(NHVal (LInt64 b))] = pure (NHVal (LInt64 (a + b)))
-    prim_int_sub    [(NHVal (LInt64 a)),(NHVal (LInt64 b))] = pure (NHVal (LInt64 (a - b)))
-    prim_int_mul    [(NHVal (LInt64 a)),(NHVal (LInt64 b))] = pure (NHVal (LInt64 (a * b)))
-    prim_int_eq     [(NHVal (LInt64 a)),(NHVal (LInt64 b))] = pure (NHVal (LBool (a == b)))
-    prim_int_print  [(NHVal (LInt64 i))] = liftIO $ print i >> pure NHUnit
+    prim_int_add    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a + b)))
+    prim_int_sub    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a - b)))
+    prim_int_mul    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a * b)))
+    prim_int_eq     [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LBool (a == b)))
+    prim_int_print  [(DVal (LInt64 i))] = liftIO $ print i >> pure DUnit
 
 -- * Test runs
 
