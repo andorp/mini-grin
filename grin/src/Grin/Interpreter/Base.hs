fa8469ea8174f63a442035e911032d8fb1a7fe1a
Andor Penzes <andor.penzes@gmail.com>
2019-07-03 00:58:46 +0200

    Add store.

--- grin/src/Grin/Interpreter/Base.hs
+++ grin/src/Grin/Interpreter/Base.hs
@@ -8,6 +8,7 @@ import Grin.Exp
 import Data.Maybe (fromJust, mapMaybe, fromMaybe)
 import Control.Monad.Trans (MonadIO(liftIO), lift)
 import Data.List (foldl')
+import Debug.Trace (traceShowId)
 
 
 newtype Env v = Env (Map.Map Name v)
@@ -22,30 +23,72 @@ lookupEnv (Env m) n = fromMaybe (error $ "Missing:" ++ show n) $ Map.lookup n m
 extendEnv :: Env v -> [(Name, v)] -> Env v
 extendEnv (Env m) vs = Env $ foldl' (\n (k,v) -> Map.insert k v n) m vs
 
-grinMain :: Program -> Exp
-grinMain (Program _ defs) = gmain
-  where
-    gmain = head $ mapMaybe (\(Def n _ b) -> if n == "main" then Just b else Nothing) defs
+newtype Store a v = Store (Map.Map a v)
+  deriving (Show)
+
+class (Ord a) => Address a where
+  addrFromInt :: Int -> a
+  intFromAddr :: a -> Int
+
+storeLookup :: (Address a) => Store a v -> a -> v
+storeLookup (Store m) a = fromMaybe (error "Store; missing") $ Map.lookup a m
+
+storeSize :: (Address a) => Store a v -> a
+storeSize (Store m) = addrFromInt (Map.size m)
+
+storeExt :: (Address a) => a -> v -> Store a v -> Store a v
+storeExt a v (Store m) = Store (Map.insert a v m)
 
 class (Monad m, MonadFail m) => Interpreter m where
-  type IntpVal m :: *
-  value       :: Val  -> m (IntpVal m) -- pure conversion, but the m type parameter needs to be present
+  type IntpVal  m :: * -- TODO: Separate Val and Node for heap
+  type IAddr    m :: *
+  -- pure conversions, but m type is needed for type inference
+  value       :: Val  -> m (IntpVal m)
+  val2addr    :: (IntpVal m) -> m (IAddr m)
+  addr2val    :: (IAddr m) -> m (IntpVal m)
+  unit        :: m (IntpVal m) -- The unit value
+  matchNode   :: (IntpVal m) -> (Tag, [Name]) -> m [(Name, IntpVal m)]
+  -- non-pure
   askEnv      :: m (Env (IntpVal m))
   localEnv    :: Env (IntpVal m) -> m (IntpVal m) -> m (IntpVal m)
   lookupFun   :: Name -> m Exp
   isOperation :: Name -> m Bool
   operation   :: Name -> [IntpVal m] -> m (IntpVal m)
   ecase       :: (Exp -> m (IntpVal m)) -> IntpVal m -> [Alt] -> m (IntpVal m)
+  getStore    :: m (Store (IAddr m) (IntpVal m))
+  updateStore :: (Store (IAddr m) (IntpVal m) -> Store (IAddr m) (IntpVal m)) -> m ()
 
-{-
-debug :: (Interpreter m, MonadIO m, Show (IntpVal m)) => (Exp -> m (IntpVal m)) -> Exp -> m (IntpVal m)
+findStore :: (Interpreter m, Address a, a ~ (IAddr m)) => (IntpVal m) -> m (IntpVal m)
+findStore l = do
+  s <- getStore
+  a <- val2addr l
+  pure $ storeLookup s a
+
+extStore :: (Interpreter m, Address a, a ~ (IAddr m)) => (IntpVal m) -> (IntpVal m) -> m ()
+extStore l n = do
+  a <- val2addr l
+  updateStore (storeExt a n)
+
+allocStore :: (Interpreter m, Address a, a ~ IAddr m) => m (IntpVal m)
+allocStore = do
+  s <- getStore
+  addr2val $ storeSize s
+
+grinMain :: Program -> Exp
+grinMain (Program _ defs) = gmain
+  where
+    gmain = head $ mapMaybe (\(Def n _ b) -> if n == "main" then Just b else Nothing) defs
+
+debug :: (Interpreter m, MonadIO m, Show v, v ~ (IntpVal m)) => (Exp -> m (IntpVal m)) -> Exp -> m (IntpVal m)
 debug ev e = do
   p <- askEnv
-  liftIO $ print p
-  ev (traceShowId e)
--}
+  liftIO $ case e of
+    SApp fn ps -> print (fn, ps)
+    _          -> pure ()
+  ev e
 
-ev :: (Interpreter m) => (Exp -> m (IntpVal m)) -> Exp -> m (IntpVal m)
+
+ev :: (Interpreter m, a ~ IAddr m, Address a) => (Exp -> m (IntpVal m)) -> Exp -> m (IntpVal m)
 ev ev = \case
   SPure n@(ConstTagNode{})  -> value n
   SPure l@(Lit{})           -> value l
@@ -66,6 +109,25 @@ ev ev = \case
         -- Call the eval function on the body of the function with the extended env
         localEnv p' (ev body)
 
+  SStore n -> do
+    p <- askEnv
+    let v = lookupEnv p n
+    a <- allocStore
+    extStore a v
+    pure a
+
+  SFetch n -> do
+    p <- askEnv
+    let v = lookupEnv p n
+    findStore v
+
+  SUpdate nl nn -> do
+    p <- askEnv
+    let vl = lookupEnv p nl
+    let vn = lookupEnv p nn
+    extStore vl vn
+    unit
+
   ECase n alts -> do
     p <- askEnv
     v <- pure $ lookupEnv p n
@@ -78,19 +140,31 @@ ev ev = \case
     let p' = extendEnv p [(n, v)]
     localEnv p' (ev rhs)
 
-eval :: (Interpreter m, MonadIO m) => Exp -> m (IntpVal m)
+  EBind lhs c@(ConstTagNode t@(Tag{}) vs) rhs -> do
+    v   <- ev lhs
+    p   <- askEnv
+    p'  <- extendEnv p <$> matchNode v (t,vs)
+    localEnv p' (ev rhs)
+
+  EBind lhs Unit rhs -> do
+    _ <- ev lhs
+    ev rhs
+
+  other -> error $ show ("ev", other)
+
+eval :: (Interpreter m, MonadIO m, a ~ IAddr m, Address a, Show v, v ~ IntpVal m) => Exp -> m v
 eval e = fix ev e
 
 -- * Test expression
 
-sump =
+add =
   Program
     []
-    [ Def "sum" ["s1", "s2"] (SApp "prim_int_add" ["s1", "s2"])
+    [ Def "add" ["s1", "s2"] (SApp "prim_int_add" ["s1", "s2"])
     , Def "main" [] $
         EBind (SPure (Lit (LInt64 10))) (Var "m1") $
         EBind (SPure (Lit (LInt64 20))) (Var "m2") $
-        SApp "sum" ["m1", "m2"]
+        SApp "add" ["m1", "m2"]
     ]
 
 fact =
@@ -112,4 +186,68 @@ fact =
         EBind (SPure (Lit (LInt64 10))) (Var "m1") $
         EBind (SApp "fact" ["m1"]) (Var "m2") $
         SApp "prim_int_print" ["m2"]
+    ]
+
+sumSimple =
+  Program
+    []
+    [ Def "main" [] $
+        EBind (SPure (Lit (LInt64 1))) (Var "m1") $
+        EBind (SPure (Lit (LInt64 100))) (Var "m2") $
+        EBind (SPure (ConstTagNode (Tag C "Int") ["m1"])) (Var "m3") $
+        EBind (SPure (ConstTagNode (Tag C "Int") ["m2"])) (Var "m4") $
+        EBind (SStore "m3") (Var "m5") $
+        EBind (SStore "m4") (Var "m6") $
+        EBind (SPure (ConstTagNode (Tag F "upto") ["m5", "m6"])) (Var "m7") $
+        EBind (SStore "m7") (Var "m8") $
+        EBind (SPure (ConstTagNode (Tag F "sum") ["m8"])) (Var "m9") $
+        EBind (SStore "m9") (Var "m10") $
+        EBind (SApp "eval" ["m10"]) (ConstTagNode (Tag C "Int") ["m11"]) $
+        SApp "prim_int_print" ["m11"]
+    , Def "upto" ["u1", "u2"] $
+        EBind (SApp "eval" ["u1"]) (ConstTagNode (Tag C "Int") ["u3"]) $
+        EBind (SApp "eval" ["u2"]) (ConstTagNode (Tag C "Int") ["u4"]) $
+        EBind (SApp "prim_int_gt" ["u3", "u4"]) (Var "u5") $
+        ECase "u5"
+          [ Alt (LitPat (LBool True)) $
+                SPure (ConstTagNode (Tag C "Nil") [])
+          , Alt (LitPat (LBool False)) $
+                EBind (SPure (Lit (LInt64 1))) (Var "u6") $
+                EBind (SApp "prim_int_add" ["u3", "u6"]) (Var "u7") $
+                EBind (SPure (ConstTagNode (Tag C "Int") ["u7"])) (Var "u8") $
+                EBind (SStore "u8") (Var "u9") $
+                EBind (SPure (ConstTagNode (Tag F "upto") ["u9", "u2"])) (Var "u10") $
+                EBind (SStore "u10") (Var "u11") $
+                SPure (ConstTagNode (Tag C "Cons") ["u1", "u11"])
+          ]
+    , Def "sum" ["s1"] $
+        EBind (SApp "eval" ["s1"]) (Var "s2") $
+        ECase "s2"
+          [ Alt (NodePat (Tag C "Nil") []) $
+                EBind (SPure (Lit (LInt64 0))) (Var "s3") $
+                SPure (ConstTagNode (Tag C "Int") ["s3"])
+          , Alt (NodePat (Tag C "Cons") ["s5", "s6"]) $
+                EBind (SApp "eval" ["s5"]) (ConstTagNode (Tag C "Int") ["s7"]) $
+                EBind (SApp "sum" ["s6"]) (ConstTagNode (Tag C "Int") ["s8"]) $
+                EBind (SApp "prim_int_add" ["s7", "s8"]) (Var "s9") $
+                SPure (ConstTagNode (Tag C "Int") ["s9"])
+          ]
+    , Def "eval" ["e1"] $
+        EBind (SFetch "e1") (Var "e2") $
+        ECase "e2"
+          [ Alt (NodePat (Tag C "Int") ["e3"]) $
+                SPure (ConstTagNode (Tag C "Int") ["e3"])
+          , Alt (NodePat (Tag C "Nil") []) $
+                SPure (ConstTagNode (Tag C "Nil") [])
+          , Alt (NodePat (Tag C "Cons") ["e4", "e5"]) $
+                SPure (ConstTagNode (Tag C "Cons") ["e4", "e5"])
+          , Alt (NodePat (Tag F "upto") ["e6", "e7"]) $
+                EBind (SApp "upto" ["e6", "e7"]) (Var "e8") $
+                EBind (SUpdate "e1" "e8") Unit $
+                SPure (Var "e8")
+          , Alt (NodePat (Tag F "sum") ["e9"]) $
+                EBind (SApp "sum" ["e9"]) (Var "e10") $
+                EBind (SUpdate "e1" "e10") Unit $
+                SPure (Var "e10")
+          ]
     ]
--- grin/src/Grin/Interpreter/Definitional.hs
+++ grin/src/Grin/Interpreter/Definitional.hs
@@ -3,115 +3,182 @@ module Grin.Interpreter.Definitional where
 
 import Control.Monad.Fail
 import Control.Monad.Trans (MonadIO(liftIO), lift)
-import Control.Monad.Trans.Reader
+import Control.Monad.Reader (MonadReader(..))
+import Control.Monad.State (MonadState(..))
+import Control.Monad.Trans.Reader hiding (ask, local)
+import Control.Monad.Trans.State hiding (state, get)
 import Data.Maybe (fromJust, mapMaybe, fromMaybe)
-import Grin.Exp
+import Grin.Exp hiding (Loc)
 import Grin.Interpreter.Base
 import Lens.Micro.Platform
 import qualified Data.Map as Map
+import Data.Text (Text, isPrefixOf, unpack)
+import Data.Word
+import Data.Int
 
 
--- * NoHeap interpreter
+-- * Definitional Interpreter
 
-
-data Node = Node Tag [Lit]
+data SVal
+  = SInt64  Int64
+  | SWord64 Word64
+  | SFloat  Float
+  | SBool   Bool
+  | SString Text
+  | SChar   Char
+  | SLoc    Loc
   deriving (Eq, Ord, Show)
+
+lit2sval :: Lit -> SVal
+lit2sval = \case
+  LInt64  i -> SInt64 i
+  LWord64 w -> SWord64 w
+  LFloat  f -> SFloat f
+  LBool   b -> SBool b
+  LString s -> SString s
+  LChar   c -> SChar c
+
+data Node = Node Tag [SVal]
+  deriving (Eq, Ord, Show)
+
+newtype Loc = Loc Int
+  deriving (Eq, Ord, Show)
+
+instance Address Loc where
+  addrFromInt = Loc
+  intFromAddr (Loc a) = a
 
 data DVal
   = DNode Node
-  | DVal  Lit
+  | DVal  SVal
   | DUnit
-  | DLoc  Int
   deriving (Eq, Ord, Show)
 
-
-data NoHeapEnv m v = NoHeapEnv
-  { _noHeapFuns :: Map.Map Name Exp
-  , _noHeapOps  :: Map.Map Name ([v] -> m v)
-  , _noHeapEnv  :: Env v
+data DefEnv m v = DefEnv
+  { _defFuns :: Map.Map Name Exp
+  , _defOps  :: Map.Map Name ([v] -> m v)
+  , _defEnv  :: Env v
   }
 
-makeLenses ''NoHeapEnv
+makeLenses ''DefEnv
 
-newtype NoHeapT m a = NoHeapT { noHeapT :: ReaderT (NoHeapEnv m DVal) m a }
-  deriving (Functor, Applicative, Monad, MonadFail, MonadIO)
+newtype DefinitionalT m a = DefinitionalT
+  { definitionalT :: StateT (Store Loc DVal) (ReaderT (DefEnv m DVal) m) a
+  }
+  deriving (Functor, Applicative, Monad, MonadFail, MonadIO, MonadReader (DefEnv m DVal), MonadState (Store Loc DVal))
 
-runNoHeapT :: (Monad m) => Exp -> [(Name, [DVal] -> m DVal)] -> NoHeapT m a -> m a
-runNoHeapT prog ops n = runReaderT (noHeapT n) noHeapEnv
+runDefinitionalT :: (Monad m) => Exp -> [(Name, [DVal] -> m DVal)] -> DefinitionalT m a -> m a
+runDefinitionalT prog ops n = runReaderT (evalStateT (definitionalT n) (Store mempty)) definitional
   where
     (Program _ defs) = prog
-    noHeapEnv = NoHeapEnv (Map.fromList ((\d@(Def n _ _) -> (n,d)) <$> defs)) (Map.fromList ops) emptyEnv
+    definitional =
+      DefEnv
+        (Map.fromList ((\d@(Def n _ _) -> (n,d)) <$> defs))
+        (Map.fromList ops)
+        emptyEnv
 
-
-instance (Applicative m, Monad m, MonadFail m) => Interpreter (NoHeapT m) where
-  type IntpVal (NoHeapT m) = DVal
-  value :: Val -> (NoHeapT m) DVal
+instance (Applicative m, Monad m, MonadFail m) => Interpreter (DefinitionalT m) where
+  type IntpVal (DefinitionalT m) = DVal
+  type IAddr   (DefinitionalT m) = Loc
+  value :: Val -> (DefinitionalT m) DVal
   value = \case
     (ConstTagNode t0 ps) -> do
       p  <- askEnv
       vs <- pure $ map (lookupEnv p) ps
-      pure $ DNode $ Node t0 $ map (\(DVal v) -> v) vs
-    (Lit n) -> pure $ DVal n
+      pure $ DNode $ Node t0 $ map (\case
+        DVal v -> v
+        other -> error $ show ("value", other)
+        ) vs
+    (Lit l) -> pure $ DVal $ lit2sval l
     Unit    -> pure $ DUnit
 
-  askEnv :: (NoHeapT m) (Env DVal)
-  askEnv = NoHeapT (_noHeapEnv <$> ask)
+  val2addr :: DVal -> (DefinitionalT m) Loc
+  val2addr (DVal (SLoc l)) = pure l
+  val2addr other           = error $ show ("val2addr", other)
 
-  localEnv :: Env DVal -> (NoHeapT m) DVal -> (NoHeapT m) DVal
-  localEnv e = NoHeapT . local (noHeapEnv %~ (const e)) . noHeapT
+  addr2val :: Loc -> (DefinitionalT m) DVal
+  addr2val = pure . DVal . SLoc
 
-  lookupFun :: Name -> (NoHeapT m) Exp
-  lookupFun funName = NoHeapT ((fromMaybe (error $ "Missing:" ++ show funName) . Map.lookup funName . _noHeapFuns) <$> ask)
+  unit :: DefinitionalT m DVal
+  unit = pure DUnit
 
-  isOperation :: Name -> (NoHeapT m) Bool
-  isOperation funName = NoHeapT ((Map.member funName . _noHeapOps) <$> ask)
+  matchNode :: DVal -> (Tag, [Name]) -> DefinitionalT m [(Name, DVal)]
+  matchNode (DNode (Node t0 vs)) (t1, ps)
+    | t0 == t1  = pure (ps `zip` (DVal <$> vs))
+    | otherwise = error "matchNode"
 
-  operation :: Name -> [DVal] -> (NoHeapT m) DVal
-  operation funName params = NoHeapT $ do
-    op <- (fromJust . Map.lookup funName . _noHeapOps) <$> ask
-    lift (op params)
+  askEnv :: (DefinitionalT m) (Env DVal)
+  askEnv = _defEnv <$> ask
 
-  ecase :: (Exp -> (NoHeapT m) DVal) -> DVal -> [Alt] -> (NoHeapT m) DVal
+  localEnv :: Env DVal -> (DefinitionalT m) DVal -> (DefinitionalT m) DVal
+  localEnv e = local (defEnv %~ (const e))
+
+  lookupFun :: Name -> (DefinitionalT m) Exp
+  lookupFun funName = (fromMaybe (error $ "Missing:" ++ show funName) . Map.lookup funName . _defFuns) <$> ask
+
+  isOperation :: Name -> (DefinitionalT m) Bool
+  isOperation funName = (Map.member funName . _defOps) <$> ask
+
+  operation :: Name -> [DVal] -> (DefinitionalT m) DVal
+  operation funName params = DefinitionalT $ do
+    op <- lift ((fromJust . Map.lookup funName . _defOps) <$> ask) -- TODO: Fix lifts
+    lift (lift (op params))
+
+  ecase :: (Exp -> (DefinitionalT m) DVal) -> DVal -> [Alt] -> (DefinitionalT m) DVal
   ecase ev v alts = evalBranch v $ head $ filter (\(Alt p b) -> match v p) alts
     where
       match :: DVal -> CPat -> Bool
+      match DUnit                 p               = error $ show ("matching failure:", DUnit, p)
+      match (DVal (SLoc l))       p               = error $ show ("matching failure:", l, p)
       match (DNode (Node t0 ps))  (NodePat t1 vs) = t0 == t1
-      match (DVal l0)             (LitPat l1)     = l0 == l1
+      match (DVal l0)             (LitPat l1)     = l0 == (lit2sval l1)
       match (DNode{})             DefaultPat      = True
       match (DVal{})              DefaultPat      = True
-      match DUnit                 p               = error $ show ("matching failure:", DUnit, p)
-      match (DLoc l)              p               = error $ show ("matching failure:", DLoc l, p)
       match _                     _               = False
 
-      evalBranch :: DVal -> Alt -> (NoHeapT m) DVal
+      evalBranch :: DVal -> Alt -> (DefinitionalT m) DVal
       evalBranch (DNode (Node t0 vs)) (Alt (NodePat t1 nps) body) = do
         p <- askEnv
         let p' = extendEnv p (nps `zip` (DVal <$> vs))
         localEnv p' (ev body)
       evalBranch _                    (Alt _               body) = ev body
 
-evalNoHeap :: (Monad m, MonadFail m, MonadIO m) => Program -> m DVal
-evalNoHeap prog = do
+  getStore :: DefinitionalT m (Store Loc DVal)
+  getStore = DefinitionalT get
+
+  updateStore :: (Store Loc DVal -> Store Loc DVal) -> DefinitionalT m ()
+  updateStore f = state (\s -> ((), f s))
+
+
+evalDefinitional :: (Monad m, MonadFail m, MonadIO m) => Program -> m DVal
+evalDefinitional prog = do
   let ops = [ ("prim_int_add", prim_int_add)
             , ("prim_int_sub", prim_int_sub)
             , ("prim_int_mul", prim_int_mul)
             , ("prim_int_print", prim_int_print)
             , ("prim_int_eq", prim_int_eq)
+            , ("prim_int_gt", prim_int_gt)
             ]
-  runNoHeapT prog ops (eval (grinMain prog))
+  runDefinitionalT prog ops (eval (grinMain prog))
   where
-    prim_int_add    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a + b)))
-    prim_int_sub    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a - b)))
-    prim_int_mul    [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LInt64 (a * b)))
-    prim_int_eq     [(DVal (LInt64 a)),(DVal (LInt64 b))] = pure (DVal (LBool (a == b)))
-    prim_int_print  [(DVal (LInt64 i))] = liftIO $ print i >> pure DUnit
+    prim_int_add    [(DVal (SInt64 a)),(DVal (SInt64 b))] = pure (DVal (SInt64 (a + b)))
+    prim_int_sub    [(DVal (SInt64 a)),(DVal (SInt64 b))] = pure (DVal (SInt64 (a - b)))
+    prim_int_mul    [(DVal (SInt64 a)),(DVal (SInt64 b))] = pure (DVal (SInt64 (a * b)))
+    prim_int_eq     [(DVal (SInt64 a)),(DVal (SInt64 b))] = pure (DVal (SBool (a == b)))
+    prim_int_gt     [(DVal (SInt64 a)),(DVal (SInt64 b))] = pure (DVal (SBool (a > b)))
+    prim_int_print  [(DVal (SInt64 i))] = liftIO $ print i >> pure DUnit
+
 
 -- * Test runs
 
-runSum :: IO ()
-runSum = do
-  print =<< evalNoHeap sump
+runAdd :: IO ()
+runAdd = do
+  print =<< evalDefinitional add
 
 runFact :: IO ()
 runFact = do
-  print =<< evalNoHeap fact
+  print =<< evalDefinitional fact
+
+runSum :: IO ()
+runSum = do
+  print =<< evalDefinitional sumSimple
