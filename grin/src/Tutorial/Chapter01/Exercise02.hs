505bd9f2855072fc67a9a7c5026a12a519c0625f
Matthias Heinzel <mheinzel@posteo.de>
2019-08-12 22:07:34 +0100

    add more primops
    Conflicts:
    	grin/src/Tutorial/Chapter01/Exercise02.hs

--- grin/src/Tutorial/Chapter01/Exercise02.hs
+++ grin/src/Tutorial/Chapter01/Exercise02.hs
@@ -38,7 +38,28 @@ type InterpretExternal = External -> [Value] -> IO Value
 
 -- Implement the external calls for the externals used in the Examples
 externalCalls :: External -> [Value] -> IO Value
+<<<<<<< HEAD
 externalCalls = undefined
+=======
+externalCalls ext args = case eName ext of
+  "prim_int_eq" -> case args of
+    [VLit (LInt64 a), VLit (LInt64 b)] -> pure $ VLit (LBool (a == b))
+    _ -> error ("prim_int_add: invalid args: " <> show args)
+  "prim_int_add" -> case args of
+    [VLit (LInt64 a), VLit (LInt64 b)] -> pure $ VLit (LInt64 (a + b))
+    _ -> error ("prim_int_add: invalid args: " <> show args)
+  "prim_int_sub" -> case args of
+    [VLit (LInt64 a), VLit (LInt64 b)] -> pure $ VLit (LInt64 (a - b))
+    _ -> error ("prim_int_sub: invalid args: " <> show args)
+  "prim_int_mul" -> case args of
+    [VLit (LInt64 a), VLit (LInt64 b)] -> pure $ VLit (LInt64 (a * b))
+    _ -> error ("prim_int_mul: invalid args: " <> show args)
+  "prim_int_print" -> case args of
+    [val@(VLit (LInt64 a))] -> val <$ print a
+    _ -> error ("prim_int_print: invalid args: " <> show args)
+  other -> error ("non-existing external: " <> show other)
+
+>>>>>>> 2e0c2b1... add more primops
 
 interpret :: InterpretExternal -> Program -> IO Value
 interpret ietx prog =
