205c2a157657904cbb3f37ffeb9adf9de73f5f40
Matthias Heinzel <mheinzel@posteo.de>
2019-08-16 10:29:52 +0100

    implement 01/01

--- grin/src/Tutorial/Chapter01/Exercise01.hs
+++ grin/src/Tutorial/Chapter01/Exercise01.hs
@@ -13,12 +13,12 @@ TODO: Write the exercise and the motivation for it
 convertGExpToExp :: forall ctx . Exp ctx -> E.Exp
 convertGExpToExp = \case
   Program  exts defs -> E.Program exts (map convertGExpToExp defs)
-  Def      n ps body -> undefined
+  Def      n ps body -> E.Def n ps (convertGExpToExp body)
   Pure     v         -> E.SPure v
   Store    n         -> E.SStore n
-  Fetch    n         -> undefined
-  Update   n v       -> undefined
-  App      n ps      -> undefined
-  Alt      c body    -> E.Alt c undefined
-  Case     n alts    -> undefined
-  Bind     lhs v rhs -> E.EBind undefined undefined undefined
+  Fetch    n         -> E.SFetch n
+  Update   n v       -> E.SUpdate n v
+  App      n ps      -> E.SApp n ps
+  Alt      c body    -> E.Alt c (convertGExpToExp body)
+  Case     n alts    -> E.ECase n (map convertGExpToExp alts)
+  Bind     lhs v rhs -> E.EBind (convertGExpToExp lhs) v (convertGExpToExp rhs)
